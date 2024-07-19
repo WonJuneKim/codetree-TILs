d865860976a050f4e6e2174fc94e89f007b57912
Banana_milk <101927445+WonJuneKim@users.noreply.github.com>
2024-07-19 21:19:57 +0900

    Title: 합과 평균의 차, Time: 176ms, Memory: 47MB, Status: Passed - CodeTree

---
+++ 240719/합과 평균의 차/sub-of-average-and-sum.js
@@ -0,0 +1,13 @@
+const fs = require("fs");
+let input = fs.readFileSync(0).toString().trim().split(" ");
+
+let a = Number(input[0]);
+let b = Number(input[1]);
+let c = Number(input[2]);
+
+let sum = a+b+c;
+let average = sum / 3;
+
+console.log(sum);
+console.log(average);
+console.log(sum-average);
