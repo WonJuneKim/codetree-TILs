f512246cee4d5fccf2f518e4c8c40ba987e5ca9c
Banana_milk <101927445+WonJuneKim@users.noreply.github.com>
2024-07-21 04:19:44 +0900

    Title: 두 사람, Time: 154ms, Memory: 14MB, Status: Wrong Answer - CodeTree

---
+++ 240721/두 사람/two-person.js
@@ -0,0 +1,14 @@
+const fs = require("fs");
+let input = fs.readFileSync(0).toString().trim().split("\n");
+
+let arr1 = input[0].split(" ");
+let arr2 = input[1].split(" ");
+
+let ageA = Number(arr1[0]);
+let genderA = arr1[1];
+let ageB = Number(arr2[0]);
+let genderB = arr2[1];
+
+if ((ageA >=19 && genderA =="M") ||(ageB >=19 && genderB =="M") ) {
+    console.log(1);
+}
