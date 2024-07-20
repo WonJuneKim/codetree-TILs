525226c71c11319b48178c69e060d33e465b7bd6
Banana_milk <101927445+WonJuneKim@users.noreply.github.com>
2024-07-21 03:18:04 +0900

    Title: 특정 조건 세 정수 비교, Time: 222ms, Memory: 14MB, Status: Passed - CodeTree

--- 240721/특정 조건 세 정수 비교/specific-comparison-of-three-natural-numbers.js
+++ 240721/특정 조건 세 정수 비교/specific-comparison-of-three-natural-numbers.js
@@ -5,7 +5,7 @@ let a = Number(input[0]);
 let b = Number(input[1]);
 let c = Number(input[2]);
 
-let resultA = (a<b && a<c) ? 1 : 0;
+let resultA = (a<=b && a<=c) ? 1 : 0;
 let resultB = (a===b & b===c) ? 1 : 0;
 
 console.log(resultA, resultB);
