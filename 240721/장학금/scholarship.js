545783b712f890c658194472fc51e0543ca3e7a7
Banana_milk <101927445+WonJuneKim@users.noreply.github.com>
2024-07-21 04:07:39 +0900

    Title: 장학금, Time: 240ms, Memory: 14MB, Status: Passed - CodeTree

--- 240721/장학금/scholarship.js
+++ 240721/장학금/scholarship.js
@@ -6,10 +6,10 @@ let final = Number(input[1]);
 
 if(middle>=90) {
     if(final >=95){
-        console.log(10);
+        console.log(100000);
     }
     else if (final >=90) {
-        console.log(5);
+        console.log(50000);
     }
     else {
         console.log(0);
