ef96867c1bd6812c054ac09c9297a4c9e002ebec
Banana_milk <101927445+WonJuneKim@users.noreply.github.com>
2024-07-24 03:06:39 +0900

    Title: 숫자의 증감, Time: 182ms, Memory: 15MB, Status: Passed - CodeTree

--- 240724/숫자의 증감/increasing-and-decreasing-numbers.js
+++ 240724/숫자의 증감/increasing-and-decreasing-numbers.js
@@ -2,7 +2,7 @@ const fs = require("fs");
 let input = fs.readFileSync(0).toString().trim().split(" ");
 
 let c = input[0];
-let n = input[1];
+let n = Number(input[1]);
 let result = "";
 
 if (c==="A") {
