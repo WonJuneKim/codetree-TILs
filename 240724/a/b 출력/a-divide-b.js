1cf50f54a75bae2bb4fd92369b2eed8844f30fa8
Banana_milk <101927445+WonJuneKim@users.noreply.github.com>
2024-07-24 02:16:53 +0900

    Title: a/b 출력, Time: 164ms, Memory: 14MB, Status: Wrong Answer - CodeTree

--- 240724/a/b 출력/a-divide-b.js
+++ 240724/a/b 출력/a-divide-b.js
@@ -4,11 +4,15 @@ let input = fs.readFileSync(0).toString().trim().split(" ");
 let a = Number(input[0]);
 let b = Number(input[1]);
 
-let result= 0;
+let result= "";
+result += parseInt(a/b) + ".";
 
+a &= b;
+for (i=1; i<20; i++) {
+    a *=10;
+    result += parseInt(a/b);
 
-for (i=1; i<=20; i++) {
-    result += parseInt(a/b)*(0.1**(i-1));
-    a = parseInt((a%b)*10);
-    console.log(result);
+    a %= b;
 }
+
+console.log(result);
