bed372edfbd1fee5d844452aa3e9554a3203c898
Banana_milk <101927445+WonJuneKim@users.noreply.github.com>
2024-10-23 23:36:58 +0900

    Title: 369 게임, Time: 165ms, Memory: 14MB, Status: Wrong Answer - CodeTree

---
+++ 241023/369 게임/369-game.js
@@ -0,0 +1,21 @@
+const fs = require("fs");
+let n = fs.readFileSync(0).toString().trim();
+
+let result ="";
+for (let i = 1; i <= n; i++) {
+    if (i%3 ===0) {
+        result += "0 ";
+    }
+    else if( i %10 ===3 || i%10 ===6 || i%10 ===9 ) {
+        result += "0 ";
+    }
+    else if(parseInt(i/10) ===3 || parseInt(i/10) ===6 || parseInt(i/9)===9  ) {
+        result += "0 ";
+    }
+    else {
+        result += i + " ";
+    }
+
+}
+
+console.log(result);
