c5f53053e4a66dab47cde87413805fae6714f3ee
Banana_milk <101927445+WonJuneKim@users.noreply.github.com>
2024-11-07 23:47:47 +0900

    Title: 5의 배수의 합, Time: 178ms, Memory: 14MB, Status: Runtime Error - CodeTree

--- 241107/5의 배수의 합/sum-of-specific-condition-multiple.js
+++ 241107/5의 배수의 합/sum-of-specific-condition-multiple.js
@@ -3,6 +3,12 @@ let input = fs.readFileSync(0).toString().trim().split(" ");
 
 let a = Number(input[0]);
 let b = Number(input[1]);
+
+if(a>b) {
+    let a = temp;
+    b =a;
+    a =temp;
+}
 let sumVal = 0;
 
 for (let i=a; i<=b; i++) {
