fdb98aff7eb699922a61d1466710af06a50db8ad
Banana_milk <101927445+WonJuneKim@users.noreply.github.com>
2024-11-30 23:51:40 +0900

    Title: 윤년 횟수 구하기, Time: 198ms, Memory: 15MB, Status: Wrong Answer - CodeTree

---
+++ 241130/윤년 횟수 구하기/number-of-leap-years.js
@@ -0,0 +1,14 @@
+const fs = require("fs");
+let input = fs.readFileSync(0).toString().trim();
+let cnt = 0;
+
+for (let i = 1; i < input; i++) {
+    if (i%4 === 0) {
+        cnt++;
+    }
+    if(i%100===0 && i%400 !==0) {
+        cnt--;
+    }
+}
+
+console.log(cnt);
