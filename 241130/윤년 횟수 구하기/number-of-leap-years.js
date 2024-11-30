60c5b2d6e7e93233a3883125b76418009778f16c
Banana_milk <101927445+WonJuneKim@users.noreply.github.com>
2024-11-30 23:52:21 +0900

    Title: 윤년 횟수 구하기, Time: 174ms, Memory: 15MB, Status: Passed - CodeTree

--- 241130/윤년 횟수 구하기/number-of-leap-years.js
+++ 241130/윤년 횟수 구하기/number-of-leap-years.js
@@ -2,7 +2,7 @@ const fs = require("fs");
 let input = fs.readFileSync(0).toString().trim();
 let cnt = 0;
 
-for (let i = 1; i < input; i++) {
+for (let i = 1; i <= input; i++) {
     if (i%4 === 0) {
         cnt++;
     }
