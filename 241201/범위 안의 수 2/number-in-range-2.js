12c9c0a0c9166f207679dcf8945d5607fbabad5a
Banana_milk <101927445+WonJuneKim@users.noreply.github.com>
2024-12-01 23:52:45 +0900

    Title: 범위 안의 수 2, Time: 168ms, Memory: 15MB, Status: Passed - CodeTree

--- 241201/범위 안의 수 2/number-in-range-2.js
+++ 241201/범위 안의 수 2/number-in-range-2.js
@@ -4,7 +4,7 @@ let input = fs.readFileSync(0).toString().trim().split("\n");
 let sumVal = 0;
 let cnt = 0;
 
-for (let i = 1; i <= 10; i++) {
+for (let i = 0; i < 10; i++) {
     let a = Number(input[i]);
     if (a>=0 && a<=200) {
         sumVal += a;
