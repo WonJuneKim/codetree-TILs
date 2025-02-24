ea78b91e1451558cb40a0a718710e55a59688716
Banana_milk <101927445+WonJuneKim@users.noreply.github.com>
2025-02-24 21:12:23 +0900

    Title: 함수를 이용한 소수 판별, Time: 176ms, Memory: 15MB, Status: Passed - CodeTree

--- 250224/함수를 이용한 소수 판별/decimal-decisions-using-functions.js
+++ 250224/함수를 이용한 소수 판별/decimal-decisions-using-functions.js
@@ -2,6 +2,7 @@ const fs = require("fs");
 let input = fs.readFileSync(0).toString().trim().split(" ");
 
 function isPrime(n) {
+    if(n<2) return false;
     for (let i = 2; i<n; i++) {
         if(n%i === 0)
         return false;
