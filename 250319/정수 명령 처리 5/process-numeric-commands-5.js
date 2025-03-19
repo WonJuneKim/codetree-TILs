feaebc88aaea84c8683cc0810ce4763d2730fc93
Banana_milk <101927445+WonJuneKim@users.noreply.github.com>
2025-03-19 09:07:31 +0900

    Title: 정수 명령 처리 5, Time: 200ms, Memory: 15MB, Status: Wrong Answer - CodeTree

---
+++ 250319/정수 명령 처리 5/process-numeric-commands-5.js
@@ -0,0 +1,26 @@
+const fs = require("fs");
+const input = fs.readFileSync(0).toString().trim().split('\n');
+
+const n = Number(input[0]);
+const commands = input.slice(1);
+
+let arr= [];
+let output = [];
+
+for(let i =0; i<n; i++){
+    const parts = commands[i].split('');
+    const cmd = parts[0];
+
+
+if(cmd === 'push back') {
+    arr.push(Number(parts[1]));
+} else if(cmd === 'pop back') {
+    arr.pop();
+} else if(cmd === 'size') {
+    output.push(arr.length);
+} else if (cmd ==='get'){
+    output.push(arr[Number(parts[1])-1]);
+}
+}
+
+console.log(output.join('\n'));
