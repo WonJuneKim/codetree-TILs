d812649152e9880f20febf8637361a4725d08684
Banana_milk <101927445+WonJuneKim@users.noreply.github.com>
2025-06-18 07:09:20 +0900

    Title: 조건에 부합하는 수, Time: 183ms, Memory: 15MB, Status: Passed - Codetree

--- 250618/조건에 부합하는 수/number-that-meets-the-condition.js
+++ 250618/조건에 부합하는 수/number-that-meets-the-condition.js
@@ -4,7 +4,7 @@ let a = Number(fs.readFileSync(0).toString());
 let result = '';
 
 for (let i =1; i<=a; i++) {
-    if((i%2===0 && i%4 !== 0) || ((i/8) %2 ===0) || (i%7 < 4))
+    if((i%2===0 && i%4 !== 0) || Math.floor(i / 8) % 2 === 0 || (i%7 < 4))
     continue;
 
     result += i + ' ';
