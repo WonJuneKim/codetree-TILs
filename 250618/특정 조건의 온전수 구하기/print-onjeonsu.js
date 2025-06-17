30042b9237f85808927043efdc748afe6712bdd1
Banana_milk <101927445+WonJuneKim@users.noreply.github.com>
2025-06-18 04:13:32 +0900

    Title: 특정 조건의 온전수 구하기, Time: 178ms, Memory: 15MB, Status: Passed - Codetree

--- 250618/특정 조건의 온전수 구하기/print-onjeonsu.js
+++ 250618/특정 조건의 온전수 구하기/print-onjeonsu.js
@@ -3,12 +3,12 @@ let n = Number(fs.readFileSync(0).toString().trim());
 
 let answer = "";
 
-for(let i =1; i<=n+1; i++) {
-    if(i%2 ===0 || String(i)[0] === '5' || (i%3 ===0 && i%9 !==0)) 
+for(let i =1; i<=n; i++) {
+    if(i%2 ===0 || i%10 ===5 || (i%3 ===0 && i%9 !==0)) 
     continue;
 
     answer += i + " ";
 }
 
-console.log(answer.trim());
+console.log(answer);
 
