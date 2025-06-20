a52ac29498ff5593fc3300b927a0bdc94ace31b5
Banana_milk <101927445+WonJuneKim@users.noreply.github.com>
2025-06-20 21:06:35 +0900

    Title: 3N + 1 수열, Time: 172ms, Memory: 15MB, Status: Passed - Codetree

--- 250620/3N + 1 수열/3n-plus-1-sequence.js
+++ 250620/3N + 1 수열/3n-plus-1-sequence.js
@@ -4,17 +4,17 @@ let n = Number(fs.readFileSync(0).toString().trim());
 let count = 0;
 
 while (true) {
+    if(n ===1) {
+        console.log(count);
+        break;
+    }
+
     if (n%2 ===0) {
         n /= 2;
         count++;
     }
-    else {
+    else{
         n = n*3 +1;
         count++;
-    }
-
-    if(n ===1) {
-        console.log(count);
-        break;
     }
 }
