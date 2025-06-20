ea60c02ddbfd9b5a13d5bed12256b6c62b46f3a0
Banana_milk <101927445+WonJuneKim@users.noreply.github.com>
2025-06-21 01:49:31 +0900

    Title: 별표 출력하기 7, Time: 178ms, Memory: 15MB, Status: Passed - Codetree

---
+++ 250621/별표 출력하기 7/print-star-7.js
@@ -0,0 +1,12 @@
+const fs = require("fs");
+let n = Number(fs.readFileSync(0).toString().trim());
+
+let str = "";
+
+for (let i =1; i<=n; i++) {
+    str ="";
+    for (let j = 1; j<=i; j++) {
+        str += "* ";
+    }
+    console.log(str);
+}
