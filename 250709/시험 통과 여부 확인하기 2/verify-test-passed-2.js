8d78855941517fd845522b8b1cb1e19573d22679
Banana_milk <101927445+WonJuneKim@users.noreply.github.com>
2025-07-09 14:31:31 +0900

    Title: 시험 통과 여부 확인하기 2, Time: 180ms, Memory: 15MB, Status: Passed - Codetree

--- 250709/시험 통과 여부 확인하기 2/verify-test-passed-2.js
+++ 250709/시험 통과 여부 확인하기 2/verify-test-passed-2.js
@@ -3,15 +3,15 @@ let input = fs.readFileSync(0).toString().trim().split("\n");
 let studentCount = Number(input[0]);
 
 
-let sum = 0;
 let cnt = 0;
 
 //학생 반복
 for(let i =0; i<studentCount; i++) {
-     sum =0;
+     let score = input[i+1].split(" ").map(Number);
+     let sum =0;
      //2번째 줄 부터 각 줄에 대한 점수 반복
      for(let j = 0; j<4; j++) {
-          let score = input[i+1].split(" ").map(Number);
+
           sum += score[j];
      }
      if ((sum/4)>=60) {
