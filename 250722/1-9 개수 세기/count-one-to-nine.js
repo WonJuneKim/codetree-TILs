aa4f89bdf37d729d8379813976a17e67213a49c5
Banana_milk <101927445+WonJuneKim@users.noreply.github.com>
2025-07-22 22:41:57 +0900

    Title: 1-9 개수 세기, Time: 173ms, Memory: 15MB, Status: Passed - Codetree

--- 250722/1-9 개수 세기/count-one-to-nine.js
+++ 250722/1-9 개수 세기/count-one-to-nine.js
@@ -9,7 +9,7 @@ let arr = Array(10).fill(0);
 
 //숫자 개수를 세는 과정
 //count 배열을 순회해서 각 배열에서 예를 들어 1이라는 값을 가지면 +1 값을 가진 arr 배열에 저장
-for (let i = 0; i<10; i++) {
+for (let i = 0; i<count.length; i++) {
     arr[Number(count[i])]++;
 }
 
