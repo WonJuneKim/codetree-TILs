d405415af143c1728f5b0888ad7ff104c9d71713
Banana_milk <101927445+WonJuneKim@users.noreply.github.com>
2025-07-22 23:23:38 +0900

    Title: 십의 자리 숫자, Time: 176ms, Memory: 15MB, Status: Passed - Codetree

--- 250722/십의 자리 숫자/number-of-tens-digit.js
+++ 250722/십의 자리 숫자/number-of-tens-digit.js
@@ -2,11 +2,16 @@ const fs = require("fs");
 let input = fs.readFileSync(0).toString().trim().split(" ").map(Number);
 
 let numArr = Array(10).fill(0);
+let i = 0;
 
-for (let i = 0; i<input.length; i++) {
+while(true) {
+
+    if(input[i] === 0) break;
+
     let a = parseInt(input[i]/10);
-    
     numArr[a]++;
+    i++;
+
 }
 
 for (let i =1; i<=9; i++) {
