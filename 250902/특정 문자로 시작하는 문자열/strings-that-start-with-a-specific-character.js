49f657187e1a19c79031b8bba6b52b2e2858e4a0
Banana_milk <101927445+WonJuneKim@users.noreply.github.com>
2025-09-02 21:16:14 +0900

    Title: 특정 문자로 시작하는 문자열, Time: 66ms, Memory: 10MB, Status: Passed - Codetree

---
+++ 250902/특정 문자로 시작하는 문자열/strings-that-start-with-a-specific-character.js
@@ -0,0 +1,17 @@
+const fs = require("fs");
+let input = fs.readFileSync(0).toString().trim().split("\n");
+
+let n = Number(input[0]);
+let str = input[n+1];
+
+let cnt = 0;
+let sum = 0;
+
+for(let i = 1; i<=n; i++) {
+    if(input[i][0] ===str) {
+        cnt++;
+        sum += input[i].length;
+    }
+}
+
+console.log(cnt, ((sum/cnt).toFixed(2)));
