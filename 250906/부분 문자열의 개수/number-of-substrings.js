5d38ff357f39e693f80433514225cf8fe7350cab
Banana_milk <101927445+WonJuneKim@users.noreply.github.com>
2025-09-06 03:58:59 +0900

    Title: 부분 문자열의 개수, Time: 63ms, Memory: 10MB, Status: Passed - Codetree

---
+++ 250906/부분 문자열의 개수/number-of-substrings.js
@@ -0,0 +1,14 @@
+const fs = require("fs");
+let input = fs.readFileSync(0).toString().trim().split("\n");
+
+let a = input[0];
+let b = input[1];
+let cnt =0;
+
+for(let i = 0; i<a.length; i++) {
+    if(a[i] ===b[0] && a[i+1] === b[1]) {
+        cnt++;
+    }
+}
+
+console.log(cnt);
