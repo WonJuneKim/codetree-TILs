3985f588755cd6d3656a7b9f7786697e45591792
Banana_milk <101927445+WonJuneKim@users.noreply.github.com>
2025-09-09 20:38:06 +0900

    Title: 그 전 알파벳, Time: 63ms, Memory: 10MB, Status: Passed - Codetree

--- 250909/그 전 알파벳/before-the-alphabet.js
+++ 250909/그 전 알파벳/before-the-alphabet.js
@@ -4,7 +4,7 @@ let input = fs.readFileSync(0).toString().trim();
 let a = input.charCodeAt(0) -1;
 
 if (input === "a") {
-    console.log(z);
+    console.log("z");
 } else { 
     console.log(String.fromCharCode(a));
 }
