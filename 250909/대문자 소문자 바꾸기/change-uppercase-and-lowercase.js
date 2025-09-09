30021b33abc7ee1f905d0fde97347228480b9e24
Banana_milk <101927445+WonJuneKim@users.noreply.github.com>
2025-09-09 22:47:20 +0900

    Title: 대문자 소문자 바꾸기, Time: 68ms, Memory: 10MB, Status: Wrong Answer - Codetree

---
+++ 250909/대문자 소문자 바꾸기/change-uppercase-and-lowercase.js
@@ -0,0 +1,16 @@
+const fs = require("fs");
+let input = fs.readFileSync(0).toString().trim();
+
+let ans = "";
+
+for(let i = 0; i<input.length; i++) {
+    let ch = input[i];
+
+    if(ch>="A" && ch<="Z") {
+        ans+= ch.toLowerCase();
+    } else if(ch>"a" && ch<="z") {
+        ans += ch.toUpperCase();
+    }
+}
+
+console.log(ans);
