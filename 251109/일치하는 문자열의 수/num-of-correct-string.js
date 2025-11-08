01e26dd1ea8574337ee554f66ef0baa8ecf3abe5
Banana_milk <101927445+WonJuneKim@users.noreply.github.com>
2025-11-09 08:48:52 +0900

    Title: 일치하는 문자열의 수, Time: 63ms, Memory: 10MB, Status: Passed - Codetree

---
+++ 251109/일치하는 문자열의 수/num-of-correct-string.js
@@ -0,0 +1,15 @@
+const fs = require("fs");
+let input = fs.readFileSync(0).toString().trim().split("\n");
+
+let n = input[0].split(" ")[0];
+
+let A = input[0].split(" ")[1];
+let cnt = 0;
+
+for (let i =1; i<=n; i++) {
+    if(input[i] === A) {
+        cnt++;
+    }
+}
+
+console.log(cnt);
