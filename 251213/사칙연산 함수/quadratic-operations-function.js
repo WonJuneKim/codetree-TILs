9b760c1309550d91fae7b3e913359faa7ec6bd31
Banana_milk <101927445+WonJuneKim@users.noreply.github.com>
2025-12-13 04:20:10 +0900

    Title: 사칙연산 함수, Time: 67ms, Memory: 10MB, Status: Wrong Answer - Codetree

---
+++ 251213/사칙연산 함수/quadratic-operations-function.js
@@ -0,0 +1,50 @@
+const fs = require("fs");
+const input = fs.readFileSync(0).toString().trim().split('\n');
+
+let [a, o, c] = input[0].split(" ");
+a = Number(a);
+c = Number(c);
+
+function sum(a, b) {
+    return a+b;
+}
+
+function subtract(a, b) {
+    return a -b;
+}
+
+function multiply(a, b) {
+    return a*b;
+}
+
+function divide(a,b) {
+    if (b ===0) return null;
+    return a/b;
+}
+
+let result;
+
+switch (o) {
+    case "+":
+    result = sum(a,c);
+    break;
+
+    case "-":
+    result= subtract(a,c);
+    break;
+
+    case "*":
+    result= multiply(a,c);
+    break;
+
+    case "/":
+    result = divide(a,c);
+    break;
+
+}
+
+if(result === undefined) {
+    console.log("False");
+} else {
+    console.log(`${a} ${o} ${c} = ${result}`);
+}
