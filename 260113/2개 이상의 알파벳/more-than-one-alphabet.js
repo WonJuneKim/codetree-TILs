b9bff262a4acf7d38737bfd0118c81fca158418d
Banana_milk <101927445+WonJuneKim@users.noreply.github.com>
2026-01-13 19:34:15 +0900

    Title: 2개 이상의 알파벳, Time: 67ms, Memory: 10MB, Status: Passed - Codetree

--- 260113/2개 이상의 알파벳/more-than-one-alphabet.js
+++ 260113/2개 이상의 알파벳/more-than-one-alphabet.js
@@ -2,12 +2,12 @@ const fs = require("fs");
 const input = fs.readFileSync(0).toString().trim().split('\n');
 const A = input[0];
 
-function checkDiff(arr) {
-    let a = arr[0];
-    for(let i =1; i<arr.length-1; i++) {
-        if(arr[i] !== a) return "Yes";
+function hasDiffChar(value) {
+    let a = value[0];
+    for(let i =1; i<value.length; i++) {
+        if(value[i] !== a) return "Yes";
     }
     return "No";
 }
 
-console.log(checkDiff(A));
+console.log(hasDiffChar(A));
