8a438d88bdc45a3302652f70b079e5df7f6bad41
Banana_milk <101927445+WonJuneKim@users.noreply.github.com>
2026-02-19 11:45:52 +0900

    Title: 나누고 빼면서 합하기, Time: 66ms, Memory: 10MB, Status: Passed - Codetree

--- 260219/나누고 빼면서 합하기/divide-and-subtract-and-add-up.js
+++ 260219/나누고 빼면서 합하기/divide-and-subtract-and-add-up.js
@@ -4,11 +4,11 @@ const [n, m] = input[0].split(" ").map(Number);
 const A = input[1].split(" ").map(Number);
 
 
-function calculate(x) {
+function calculate(x,arr) {
     let sum = 0;
 
     while (x>=1) {
-        sum += A[x-1];
+        sum += arr[x-1];
 
         if(x%2 !==0) {
             x -= 1;
@@ -20,4 +20,4 @@ function calculate(x) {
     return sum;
 }
 
-console.log(calculate(m));
+console.log(calculate(m,A));
