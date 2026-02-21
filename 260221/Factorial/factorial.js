96d8f4d07bbefd34df79f8d6ef18f37c168e69b1
Banana_milk <101927445+WonJuneKim@users.noreply.github.com>
2026-02-21 15:25:23 +0900

    Title: Factorial, Time: 67ms, Memory: 10MB, Status: Passed - Codetree

---
+++ 260221/Factorial/factorial.js
@@ -0,0 +1,11 @@
+const fs = require("fs");
+const input = fs.readFileSync(0).toString().trim().split('\n');
+const N = Number(input[0]);
+
+function factorial(n) {
+    if(n===1) return 1;
+
+    return factorial(n-1) * n;
+}
+
+console.log(factorial(N));
