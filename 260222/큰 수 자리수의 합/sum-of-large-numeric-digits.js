96758b78b4a46dbc1657b2f429d15dccaaf7b011
Banana_milk <101927445+WonJuneKim@users.noreply.github.com>
2026-02-22 17:29:35 +0900

    Title: 큰 수 자리수의 합, Time: 65ms, Memory: 10MB, Status: Passed - Codetree

---
+++ 260222/큰 수 자리수의 합/sum-of-large-numeric-digits.js
@@ -0,0 +1,11 @@
+const fs = require("fs");
+const input = fs.readFileSync(0).toString().trim().split('\n');
+let [a, b, c] = input[0].split(" ").map(Number);
+
+function f(n) {
+    if(n <10) return n;
+
+    return f(Math.floor(n/10)) + (n%10);
+}
+
+console.log(f(a*b*c));
