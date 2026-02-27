a6eb3064efefc241867e32fe6242f0bf6b2fbbca
Banana_milk <101927445+WonJuneKim@users.noreply.github.com>
2026-02-28 03:08:03 +0900

    Title: 폭탄 해제, Time: 66ms, Memory: 11MB, Status: Passed - Codetree

---
+++ 260228/폭탄 해제/bomb-removal.js
@@ -0,0 +1,17 @@
+const fs = require("fs");
+const input = fs.readFileSync(0).toString().trim().split('\n');
+const [u_code, l_color, time] = input[0].split(' ');
+
+class Bomb {
+    constructor (code, color, time) {
+    this.code = code;
+    this.color = color;
+    this.time = time;
+    }
+}
+
+const bomb = new Bomb(u_code, l_color, time);
+
+console.log(`code : ${bomb.code}`);
+console.log(`color : ${bomb.color}`);
+console.log(`second : ${bomb.time}`);
