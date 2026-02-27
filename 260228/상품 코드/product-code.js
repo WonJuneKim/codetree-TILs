c9983855ce4ff5241907dc683eabfd5592e12117
Banana_milk <101927445+WonJuneKim@users.noreply.github.com>
2026-02-28 03:15:14 +0900

    Title: 상품 코드, Time: 62ms, Memory: 10MB, Status: Passed - Codetree

---
+++ 260228/상품 코드/product-code.js
@@ -0,0 +1,16 @@
+const fs = require("fs");
+const input = fs.readFileSync(0).toString().trim().split('\n');
+const [prod2_id, code2] = input[0].split(' ');
+
+class Product {
+    constructor (name="codetree", code = 50) {
+        this.name = name;
+        this.code = code;
+    }
+}
+
+const product1 = new Product();
+const product2 = new Product(prod2_id, code2);
+
+console.log(`product ${product1.code} is ${product1.name}`);
+console.log(`product ${product2.code} is ${product2.name}`);
