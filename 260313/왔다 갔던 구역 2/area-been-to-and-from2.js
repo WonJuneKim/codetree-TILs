94e11f823c8f9ea70280fb4d1394ecdecfa8eaaf
Banana_milk <101927445+WonJuneKim@users.noreply.github.com>
2026-03-13 16:36:06 +0900

    Title: 왔다 갔던 구역 2, Time: 64ms, Memory: 16MB, Status: Runtime Error - Codetree

---
+++ 260313/왔다 갔던 구역 2/area-been-to-and-from2.js
@@ -0,0 +1,41 @@
+const fs = require("fs");
+const input = fs.readFileSync(0).toString().trim().split('\n');
+
+const n = Number(input[0]);
+const commands = input.slice(1, n + 1);
+
+//배열을 생성하고, 지나갈때 마다 1씩 추가 
+//마지막에 2이상인 곳을 카운팅
+
+const size = 3000;
+const OFFSET = 1500;
+
+let diff = Array(size).fill(0);
+
+let cur = 0;
+
+for(const lines of commands) {
+    const [xStr, dir] = lines.split(" ");
+    const x = Number(xStr);
+
+    let next = dir === "R" ? cur + x : cur - x;
+
+    let a = Math.min(cur, next);
+    let b = Math.max(cur, next);
+
+    diff[a+OFFSET] += 1;
+    diff[b+OFFSET] -= 1;
+
+    cur = next;
+}
+
+for(let i =0; i<size; i++) {
+    diff[i+1] += diff[i];
+}
+
+let answer = 0;
+for(let i =0; i<dsize; i++) {
+    if(diff[i]>=2) answer++;
+}
+
+console.log(answer);
