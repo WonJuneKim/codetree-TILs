27e090611d6c3c96971e6c8bff7ef94f2b9c4f8c
Banana_milk <101927445+WonJuneKim@users.noreply.github.com>
2026-03-15 18:29:48 +0900

    Title: 사각형의 총 넓이 2, Time: 60ms, Memory: 12MB, Status: Runtime Error - Codetree

---
+++ 260315/사각형의 총 넓이 2/total-width-of-a-rectangle2.js
@@ -0,0 +1,26 @@
+const fs = require("fs");
+const input = fs.readFileSync(0).toString().trim().split('\n');
+
+const n = Number(input[0]);
+const rects = input.slice(1, n + 1).map(line => line.split(' ').map(Number));
+
+const SIZE = 100;
+const sheets = Array.from({length: SIZE}, ()=>Array(SIZE).fill(0));
+
+for(let [x,y,a,b] of rects) {
+    for(let i =x; i<a; i++) {
+        for(let j = y; j<b; j++) {
+            sheets[i][j] =1;
+        }
+    }
+}
+
+let sum =0;
+
+for(let i =0; i<SIZE; i++) {
+    for(let j =0; j<SIZE; j++) {
+        if(sheets[i][j] === 1) sum++;
+    }
+}
+
+console.log(sum);
