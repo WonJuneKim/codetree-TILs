f474561f8fb0515ef725b8ef09b91911e6c7976b
Banana_milk <101927445+WonJuneKim@users.noreply.github.com>
2026-03-15 18:36:59 +0900

    Title: 사각형의 총 넓이 2, Time: 71ms, Memory: 10MB, Status: Passed - Codetree

--- 260315/사각형의 총 넓이 2/total-width-of-a-rectangle2.js
+++ 260315/사각형의 총 넓이 2/total-width-of-a-rectangle2.js
@@ -5,12 +5,17 @@ const n = Number(input[0]);
 const rects = input.slice(1, n + 1).map(line => line.split(' ').map(Number));
 
 const OFFSET = 100;
-const SIZE = 2* OFFSET+1;
+const SIZE = 2* OFFSET;
 const sheets = Array.from({length: SIZE}, ()=>Array(SIZE).fill(0));
 
 for(let [x,y,a,b] of rects) {
-    for(let i =x; i<a; i++) {
-        for(let j = y; j<b; j++) {
+    const sx = x + OFFSET;
+    const sy = y + OFFSET;
+    const ex = a + OFFSET;
+    const ey = b + OFFSET;
+
+    for(let i =sx; i<ex; i++) {
+        for(let j = sy; j<ey; j++) {
             sheets[i][j] =1;
         }
     }
