3930dd7cf8a8b6149da1bb7f375b5e56db6c23ea
Banana_milk <101927445+WonJuneKim@users.noreply.github.com>
2026-03-21 20:21:31 +0900

    Title: 연속되는 수 3, Time: 70ms, Memory: 10MB, Status: Wrong Answer - Codetree

---
+++ 260321/연속되는 수 3/continuous-number3.js
@@ -0,0 +1,18 @@
+const fs = require("fs");
+const input = fs.readFileSync(0).toString().trim().split('\n');
+
+const n = Number(input[0]);
+const arr = input.slice(1, n + 1).map(Number);
+
+let ans = 0;
+let cnt = 1;
+for(let i =1; i<n; i++) {
+    if(arr[i]*arr[i-1] >0) {
+        cnt++;
+    } else {
+        cnt =1;
+    }
+    ans = Math.max(ans,cnt);
+}
+
+console.log(ans);
